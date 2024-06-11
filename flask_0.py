a555759dad81e020f79b722d280f826b9bf6d05b
Fevelaro <felipe.velasquez.robledo@gmail.com>
2024-06-11 01:11:27 -0400

    Video 10, actalización de flask_0 con template, jinja basico y renderizar templates bàsico

--- flask_0.py
+++ flask_0.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
+
+items = ["Item 1", "Item 2", "Item 3", "Item 4"]
 
 @app.route("/index")
 def index():
@@ -11,5 +13,9 @@ def index():
 @app.route("/show_information_address")
 def show_information():
     user_ip=request.cookies.get("user_ip_information")
-    return "Hola tu dirección ip es "+user_ip
+    context = {
+        "user_ip":user_ip,
+        "items":items
+    }
+    return render_template("ip_information.html", **context)
 app.run(host='0.0.0.0',port=3000, debug=True)
